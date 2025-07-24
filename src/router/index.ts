6a070d8bb2041445129919d8bf589e77082ec840
alex <amoreno@mercalia.com>
2025-07-24 10:43:18 +0200

    hash

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import ItineraryView from '../views/ItineraryView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
